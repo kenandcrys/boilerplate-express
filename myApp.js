ea7150e20fcd9da4996fb6e76cfd001c5394346a
Kenneth Fisher <kenandcrys@live.com>
2023-10-25 16:19:31 -0500

    API Done

--- myApp.js
+++ myApp.js
@@ -1,26 +1,55 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 const path = require('path');
-
+require("dotenv").config();
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-app.get("/", function(req, res) {
+app.use((req, res, next) => {
+    const logString = `${req.method} ${req.path} - ${req.ip}`;
+    console.log(logString);
+    next();
+});
+
+app.use('/now', (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+});
+
+app.get('/now', (req, res) => {
+    res.json({ time: req.time });
+});
+
+app.get('/:word/echo', (req, res) => {
+    const word = req.params.word;
+    res.json({ echo: word });
+});
+
+app.route('/name')
+    .get((req, res) => {
+        const { first, last } = req.query;
+        const fullName = `${first} ${last}`;
+        res.json({ name: fullName });
+    })
+    .post((req, res) => {
+        const { first, last } = req.body;
+        const fullName = `${first} ${last}`;
+        res.json({ name: fullName });
+    });
+
+
+
+app.get("/", (req, res) => {
     res.status(200).sendFile(__dirname + "/views/index.html");
 });
 
-app.get('/json', (req, res) => {
-    let message = "Hello json";
-
-    if (process.env.MESSAGE_STYLE === 'uppercase') {
-        res.status(200).json({
-            message: message.toUpperCase()
-        });
+app.get("/json", (req, res) => {
+    if (process.env["MESSAGE_STYLE"] === "uppercase") {
+        res.json({ "message": "HELLO JSON" });
     } else {
-        res.status(200).json({
-            message: message
-        });
+        res.json({ "message": "Hello json" });
     }
 });
-
 
 module.exports = app;
