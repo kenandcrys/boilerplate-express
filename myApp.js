2b2ef700f6093d778175cb79a762ec6247a80408
Kenneth Fisher <kenandcrys@live.com>
2023-10-25 13:28:47 -0500

    another

--- myApp.js
+++ myApp.js
@@ -6,12 +6,21 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get("/", function(req, res) {
     res.status(200).sendFile(__dirname + "/views/index.html");
-  });
+});
 
-app.get('/json', (req,res) => {
-    res.status(200).json({
-        message: "Hello json"
-    })
-})
+app.get('/json', (req, res) => {
+    let message = "Hello json";
 
- module.exports = app;
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        res.status(200).json({
+            message: message.toUpperCase()
+        });
+    } else {
+        res.status(200).json({
+            message: message
+        });
+    }
+});
+
+
+module.exports = app;
