baff419a40ee56d418b28b9d76a1efbd6133b6c8
Kenneth Fisher <kenandcrys@live.com>
2023-10-25 13:10:55 -0500

    third

--- myApp.js
+++ myApp.js
@@ -1,10 +1,17 @@
 let express = require('express');
 let app = express();
+const path = require('path');
 
-console.log("Hello World");
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get("/", function(req, res) {
-    res.status(200).sendFile(__dirname + "views/index.html");
+    res.status(200).sendFile(__dirname + "/views/index.html");
   });
+
+app.get('/json', (req,res) => {
+    res.status(200).json({
+        message: "Hello json"
+    })
+})
 
  module.exports = app;
